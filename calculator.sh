b9b9f5a4974e1924b03b72b1e4f2bcf3aaed4e8c
Alternade <aptsahu2@gmail.com>
2021-11-22 18:05:27 +0530

    Added Substraction feature

--- calculator.sh
+++ calculator.sh
@@ -1,7 +1,9 @@
-echo "Enter a number"
-read a
-echo "Enter 2nd number"
-read b
-sum=$(($a+$b))
-echo "sum is $sum"
+echo "Enter a number" 
+read a 
+echo "Enter 2nd number" 
+read b 
+sum=$(($a+$b)) 
+echo "sum is $sum" 
+sub=$(($a-$b)) 
+echo "sub is $sub"
 
